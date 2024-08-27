37f30b853a9bfe79bb0a40525ba3ce8955aeedab
ethanG <ethangunzb@gmail.com>
2024-08-27 14:49:23 +0200

    gestion des stock fini

--- back-office/src/Views/stock/ShowStock.jsx
+++ back-office/src/Views/stock/ShowStock.jsx
@@ -12,7 +12,17 @@ const ProductList = () => {
   const [selectedProducts, setSelectedProducts] = useState([]);
   const [productTypes, setProductTypes] = useState([]);
   const [warehouses, setWarehouses] = useState([]);
+  const [viewProduct, setViewProduct] = useState(null); // État pour le produit à afficher
   const { t } = useTranslation("global");
+
+  const handleViewProduct = (product) => {
+    setViewProduct(product);
+  };
+
+  // Fonction pour fermer la modale
+  const handleCloseModal = () => {
+    setViewProduct(null);
+  };
 
   const fetchProducts = async () => {
     try {
@@ -293,7 +303,7 @@ const ProductList = () => {
                     </button>
                   </>
                 )}
-                <button className="font-medium text-purple-600 dark:text-red-500 hover:underline ml-2">
+                <button type="button" onClick={() => handleViewProduct(product)}className="font-medium text-purple-600 dark:text-red-500 hover:underline ml-2">
                       {t('view')}
                 </button>
               </td>
@@ -301,6 +311,27 @@ const ProductList = () => {
           ))}
         </tbody>
       </table>
+        {viewProduct && (
+          <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-50">
+            <div className="bg-white p-6 rounded-lg shadow-lg w-4/5 max-w-2xl relative">
+              <h2 className="text-xl font-semibold mb-4">{t('Product Details')}</h2>
+              <button onClick={handleCloseModal} className="absolute top-2 right-2 text-gray-500 hover:text-gray-800">
+                <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path>
+                </svg>
+              </button>
+              <div className="space-y-4">
+                <p><strong>{t('Product Name')}:</strong> {viewProduct.product_name}</p>
+                <p><strong>{t('Product Type')}:</strong> {viewProduct.product_type.product_type}</p>
+                <p><strong>{t('Description')}:</strong> {viewProduct.description}</p>
+                <p><strong>{t('Quantity')}:</strong> {viewProduct.quantity}</p>
+                <p><strong>{t('Expiration Date')}:</strong> {viewProduct.expiration_date}</p>
+                <p><strong>{t('Warehouse')}:</strong> {viewProduct.warehouse?.warehouse_name || t('N/A')}</p>
+                <p><strong>{t('Barcode')}:</strong> {viewProduct.barcode}</p>
+              </div>
+            </div>
+          </div>
+        )}
     </div>
   );
 };
